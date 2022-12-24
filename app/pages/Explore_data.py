a53295151294b6bd74f121e922c19779aaa55c12
bncszb <bszabo96@gmail.com>
2022-12-24 16:21:46 +0100

    asd

--- app/pages/Explore_data.py
+++ app/pages/Explore_data.py
@@ -20,16 +20,19 @@ if "events" not in st.session_state:
     events["event_date"]=events["event_date"].str.replace("-","").str.strip()
     events["event_date"]=pd.to_datetime(events["event_date"], format='%Y.%m.%d. %H:%M')
     events["date"]=events["event_date"].dt.date
+    events["month"]=events["event_date"].dt.month
+    events["weekday"]=events["event_date"].dt.weekday
+    events["hour"]=events["event_date"].dt.hour
 
     events["longitude"]=events["longitude"].astype("float32")
     events["latitude"]=events["latitude"].astype("float32")
 
-    st.session_state=events
+    st.session_state.events=events
 
 
 def create_map():
 
-    events_in_cat=events[(events["longitude"]!=0) & (events["latitude"]!=0) & (events["categoryName"].isin(categories))]
+    events_in_cat=st.session_state.events[(st.session_state.events["longitude"]!=0) & (st.session_state.events["latitude"]!=0) & (st.session_state.events["categoryName"].isin(categories))]
     fig = px.density_mapbox(events_in_cat, lat='latitude', lon='longitude', radius=10, hover_data=["date","categoryName","subCategoryName", "title"],
                             center=dict(lat=47.2, lon=19.5), zoom=6.5,
                             mapbox_style="stamen-terrain", width=1200, height=800)
@@ -41,7 +44,7 @@ if __name__ == '__main__':
         layout="wide"
     )
 
-    categories=st.multiselect("Select categories",events.categoryName.unique())
+    categories=st.multiselect("Select categories",st.session_state.events.categoryName.unique())
 
     fig=create_map()
     st.plotly_chart(fig)
